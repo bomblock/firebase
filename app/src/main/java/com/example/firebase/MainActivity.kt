09f39d9c8d06e9454e3b0ea79201b08466ccf1c0
bomblock <ekwotmd@gmail.com>
2023-04-13 22:11:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.noEmailLoginBtn)
+        btn.setOnClickListener {
+            
+        }
+    }
+}
